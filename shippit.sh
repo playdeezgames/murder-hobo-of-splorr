736dbb943c6d76ff95571916eaa3bd89405072d2
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-12 05:11:00 -0600

    updated shippit for property destination

--- shippit.sh
+++ shippit.sh
@@ -4,8 +4,8 @@ rm -rf ./pub-mac
 dotnet publish ./src/SAROS/SAROS.vbproj -o ./pub-linux -c Release --sc -p:PublishSingleFile=true -r linux-x64
 dotnet publish ./src/SAROS/SAROS.vbproj -o ./pub-windows -c Release --sc -p:PublishSingleFile=true -r win-x64
 dotnet publish ./src/SAROS/SAROS.vbproj -o ./pub-mac -c Release --sc -p:PublishSingleFile=true -r osx-x64
-butler push pub-windows thegrumpygamedev/solitary-ancient-ruins-of-splorr:windows
-butler push pub-linux thegrumpygamedev/solitary-ancient-ruins-of-splorr:linux
-butler push pub-mac thegrumpygamedev/solitary-ancient-ruins-of-splorr:mac
+butler push pub-windows thegrumpygamedev/murder-hobo-of-splorr:windows
+butler push pub-linux thegrumpygamedev/murder-hobo-of-splorr:linux
+butler push pub-mac thegrumpygamedev/murder-hobo-of-splorr:mac
 git add -A
 git commit -m "shipped it!"
