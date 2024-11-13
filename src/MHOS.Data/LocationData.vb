560c607197869a0c7217cb1fd41e954bb570dacb
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-13 03:08:43 -0600

    cleaned out location data

--- src/MHOS.Data/LocationData.vb
+++ src/MHOS.Data/LocationData.vb
@@ -1,6 +1,4 @@
 ﻿Public Class LocationData
-    Property Neighbors As New Dictionary(Of String, Integer)
-    Property Doors As New Dictionary(Of String, String)
     Property Routes As New Dictionary(Of String, RouteData)
     Property Characters As New HashSet(Of Integer)
     Property LocationType As String
