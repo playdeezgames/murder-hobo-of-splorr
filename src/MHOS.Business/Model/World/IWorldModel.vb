e8cafc7608680df519b14ca93fd51620a7f322e1
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-13 12:31:19 -0600

    duplicated avatarmodel in worldmodel

--- src/MHOS.Business/Model/World/IWorldModel.vb
+++ src/MHOS.Business/Model/World/IWorldModel.vb
@@ -1,4 +1,8 @@
 ﻿Public Interface IWorldModel
     ReadOnly Property Session As IWorldSessionModel
     ReadOnly Property Avatar As IAvatarModel
+    ReadOnly Property Description As IEnumerable(Of (Text As String, Mood As String))
+    ReadOnly Property CanEnterGameMenu As Boolean
+    ReadOnly Property AvailableChoices As (Text As String, Choice As String)()
+    Sub MakeChoice(choice As String)
 End Interface
--- src/MHOS.Business/Model/World/WorldModel.vb
+++ src/MHOS.Business/Model/World/WorldModel.vb
@@ -3,7 +3,14 @@
 
     Private _world As IWorld
     Private ReadOnly _options As IEmbarkOptions = New EmbarkOptions()
+    Private choiceMode As String = ChoiceModes.Navigation
     Sub New()
+    End Sub
+
+    Public Sub MakeChoice(choice As String) Implements IWorldModel.MakeChoice
+        choiceMode = ChoiceModes.
+            Descriptors(choiceMode).
+            MakeChoice(World, choice)
     End Sub
 
     Private Property World As IWorld
@@ -22,6 +29,27 @@
     Public ReadOnly Property Session As IWorldSessionModel Implements IWorldModel.Session
         Get
             Return New WorldSessionModel(Sub(w) World = w, Function() World, _options)
+        End Get
+    End Property
+
+    Public ReadOnly Property Description As IEnumerable(Of (Text As String, Mood As String)) Implements IWorldModel.Description
+        Get
+            Return ChoiceModes.Descriptors(choiceMode).Description(World)
+        End Get
+    End Property
+
+    Public ReadOnly Property CanEnterGameMenu As Boolean Implements IWorldModel.CanEnterGameMenu
+        Get
+            Return ChoiceModes.Descriptors(choiceMode).CanEnterGameMenu
+        End Get
+    End Property
+
+    Public ReadOnly Property AvailableChoices As (Text As String, Choice As String)() Implements IWorldModel.AvailableChoices
+        Get
+            Return ChoiceModes.
+                Descriptors(choiceMode).
+                AvailableChoices(World).
+                Select(Function(x) (Choices.Descriptors(x).Text, x)).ToArray
         End Get
     End Property
 End Class
