636419361dda7d72c62744de5538651b0137ca77
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-14 04:56:37 -0600

    characterdata client derives from entity

--- src/MHOS.Persistence.Implementation/CharacterDataClient.vb
+++ src/MHOS.Persistence.Implementation/CharacterDataClient.vb
@@ -1,17 +1,14 @@
 ﻿Imports MHOS.Data
 
 Friend MustInherit Class CharacterDataClient
-    Inherits Entity(Of CharacterData)
-    Protected ReadOnly worldData As WorldData
-    Protected ReadOnly EntityId As Integer
-    Protected ReadOnly Property EntityData As CharacterData
+    Inherits Entity(Of CharacterData, Integer)
+    Protected Overrides ReadOnly Property EntityData As CharacterData
         Get
-            Return worldData.Characters(EntityId)
+            Return WorldData.Characters(EntityId)
         End Get
     End Property
 
     Public Sub New(worldData As Data.WorldData, entityId As Integer)
-        Me.worldData = worldData
-        Me.EntityId = entityId
+        MyBase.New(worldData, entityId)
     End Sub
 End Class
--- src/MHOS.Persistence.Implementation/Entity.vb
+++ src/MHOS.Persistence.Implementation/Entity.vb
@@ -1,3 +1,12 @@
-﻿Class Entity(Of TEntityData)
+﻿Imports MHOS.Data
+
+Friend MustInherit Class Entity(Of TEntityData, TIdentifier)
     Implements IEntity
+    Protected ReadOnly WorldData As WorldData
+    Protected ReadOnly EntityId As TIdentifier
+    Sub New(worldData As WorldData, entityId As TIdentifier)
+        Me.WorldData = worldData
+        Me.EntityId = entityId
+    End Sub
+    Protected MustOverride ReadOnly Property EntityData As TEntityData
 End Class
