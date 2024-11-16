33a9461e5a9457f8ad43566f3e89f9df2f4536ab
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-16 05:00:43 -0600

    attack bonuses

--- src/MHOS.Business/Enums/Classes/ClassLevels/ClassLevelDescriptor.vb
+++ src/MHOS.Business/Enums/Classes/ClassLevels/ClassLevelDescriptor.vb
@@ -3,6 +3,7 @@
     ReadOnly Property HitDice As Integer
     ReadOnly Property HitPoints As Integer
     ReadOnly Property ExperiencePoints As Integer
+    ReadOnly Property AttackBonus As Integer
     ReadOnly Property HitDieRollCounterType As String
         Get
             Return CounterTypes.LevelHitDieRoll(Level)
@@ -13,10 +14,16 @@
             Return HitDice > 0
         End Get
     End Property
-    Sub New(level As Integer, hitDice As Integer, hitPoints As Integer, experiencePoints As Integer)
+    Sub New(
+           level As Integer,
+           hitDice As Integer,
+           hitPoints As Integer,
+           experiencePoints As Integer,
+           attackBonus As Integer)
         Me.Level = level
         Me.HitDice = hitDice
         Me.HitPoints = hitPoints
         Me.ExperiencePoints = experiencePoints
+        Me.AttackBonus = attackBonus
     End Sub
 End Class
