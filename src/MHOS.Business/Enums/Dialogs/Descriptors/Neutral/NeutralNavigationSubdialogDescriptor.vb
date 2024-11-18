ceec8c9b4db6df44abec40acefedee09281cbd4c
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-18 04:03:36 -0600

    subdialogs in neutral for better separation

--- src/MHOS.Business/Enums/Dialogs/Descriptors/Neutral/NeutralNavigationSubdialogDescriptor.vb
+++ src/MHOS.Business/Enums/Dialogs/Descriptors/Neutral/NeutralNavigationSubdialogDescriptor.vb
@@ -1,16 +1,15 @@
-﻿Friend Class NeutralDialogDescriptor
+﻿Friend Class NeutralNavigationSubdialogDescriptor
     Inherits BaseDialogDescriptor
 
     Public Sub New()
         MyBase.New(Dialogs.Neutral)
     End Sub
 
+    Public Overrides Function GoBackDialog(world As IWorld) As String
+        Return Nothing
+    End Function
+
     Public Overrides Function AvailableChoices(world As IWorld) As IEnumerable(Of String)
-        If world.Avatar Is Nothing Then
-            Return {
-                Choices.Initialize
-                }
-        End If
         Return {
                 Choices.MoveMenu,
                 Choices.Status
@@ -18,11 +17,6 @@
     End Function
 
     Public Overrides Function Description(world As IWorld) As IEnumerable(Of (Text As String, Mood As String))
-        If world.Avatar Is Nothing Then
-            Return {
-                ("The world is without form and void.", Moods.Normal)
-                }
-        End If
         Dim result As New List(Of (Text As String, Mood As String))
         Dim avatar = world.Avatar
         For Each message In avatar.Messages
@@ -34,12 +28,5 @@
             result.Add(($"{route.RouteTypeName} going {route.DirectionName}.", Moods.Normal))
         Next
         Return result
-    End Function
-
-    Public Overrides Function GoBackDialog(world As IWorld) As String
-        If world.Avatar Is Nothing Then
-            Return Dialogs.Neutral
-        End If
-        Return Nothing
     End Function
 End Class
---
+++ src/MHOS.Business/Enums/Dialogs/Descriptors/Neutral/NeutralUninitializedSubdialogDescriptor.vb
@@ -0,0 +1,23 @@
+﻿Friend Class NeutralUninitializedSubdialogDescriptor
+    Inherits BaseDialogDescriptor
+
+    Public Sub New()
+        MyBase.New(Dialogs.Neutral)
+    End Sub
+
+    Public Overrides Function GoBackDialog(world As IWorld) As String
+        Return Dialogs.Neutral
+    End Function
+
+    Public Overrides Function AvailableChoices(world As IWorld) As IEnumerable(Of String)
+        Return {
+                Choices.Initialize
+                }
+    End Function
+
+    Public Overrides Function Description(world As IWorld) As IEnumerable(Of (Text As String, Mood As String))
+        Return {
+                ("The world is without form and void.", Moods.Normal)
+                }
+    End Function
+End Class
