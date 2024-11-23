7b6bf8444d1954c897298ef9b8f21e1942e5891e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-23 07:48:57 -0600

    InitializeDialog

--- src/MHOS.Business/Dialogs/InitializeDialog.vb
+++ src/MHOS.Business/Dialogs/InitializeDialog.vb
@@ -7,17 +7,28 @@
 
     Public Overrides ReadOnly Property Description As IEnumerable(Of (Text As String, Mood As String))
         Get
-            Return Dialogs.Descriptors(Dialogs.Initialize).Description(World)
+            World.DoNextStep()
+            Return {
+                ($"Steps Remaining: {World.InitializationStepCount}", Moods.Normal)
+            }
         End Get
     End Property
 
     Public Overrides ReadOnly Property AvailableChoices As IChoice()
         Get
-            Return Dialogs.Descriptors(Dialogs.Initialize).AvailableChoices(World).ToArray
+            Return {
+            New NextChoice(
+            If(
+                World.InitializationStepCount = 0,
+                CType(New ChooseRaceDialog(World), IDialog),
+                New InitializeDialog(World)),
+            Dialogs.Initialize,
+            World)
+            }
         End Get
     End Property
 
     Public Overrides Function GoBack() As IDialog
-        Return Dialogs.Descriptors(Dialogs.Initialize).GoBackDialog(World)
+        Return Me
     End Function
 End Class
