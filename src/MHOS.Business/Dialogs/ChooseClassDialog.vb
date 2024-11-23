3424da2e6214a7e2f94100aa926ff9e06cd04afa
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-23 07:44:00 -0600

    ChooseClassDialog

--- src/MHOS.Business/Dialogs/ChooseClassDialog.vb
+++ src/MHOS.Business/Dialogs/ChooseClassDialog.vb
@@ -7,17 +7,21 @@
 
     Public Overrides ReadOnly Property Description As IEnumerable(Of (Text As String, Mood As String))
         Get
-            Return Dialogs.Descriptors(Dialogs.ChooseClass).Description(World)
+            Dim result As New List(Of (Text As String, Mood As String)) From {
+            ($"Race: {World.Avatar.RaceName}", Moods.Normal)
+        }
+            result.AddRange(World.Avatar.DescribeAttributes)
+            Return result
         End Get
     End Property
 
     Public Overrides ReadOnly Property AvailableChoices As IChoice()
         Get
-            Return Dialogs.Descriptors(Dialogs.ChooseClass).AvailableChoices(World).ToArray
+            Return Classes.Descriptors.Values.Where(Function(x) x.IsQualified(World.Avatar)).Select(Function(x) New ClassChoice(x.Class, Dialog, World))
         End Get
     End Property
 
     Public Overrides Function GoBack() As IDialog
-        Return Dialogs.Descriptors(Dialogs.ChooseClass).GoBackDialog(World)
+        Return Me
     End Function
 End Class
