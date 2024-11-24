01294405914dc2d39258d5eb4674d3c7ef35d857
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-11-24 05:15:14 -0600

    winnow down flag types

--- src/MHOS.Business/Enums/Properties/FlagTypes/FlagTypeDescriptor.vb
+++
@@ -1,6 +0,0 @@
-﻿Friend Class FlagTypeDescriptor
-    ReadOnly Property FlagType As String
-    Sub New(flagType As String)
-        Me.FlagType = flagType
-    End Sub
-End Class
--- src/MHOS.Business/Enums/Properties/FlagTypes/TownGateFlagTypeDescriptor.vb
+++
@@ -1,6 +0,0 @@
-﻿Friend Class TownGateFlagTypeDescriptor
-    Inherits FlagTypeDescriptor
-    Public Sub New(direction As String)
-        MyBase.New(FlagTypes.TownGateDirection(direction))
-    End Sub
-End Class
